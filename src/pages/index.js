7c53233eafe314a0a59349e7e9e672e4d78a9efd
Osvaldo Mejia <arturoosvaldomejiasoto@gmail.com>
2022-06-20 07:31:42 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Layout from "../../components/Layout";
+import { gql, useQuery } from '@apollo/client'
+const Index = () => {
+    return (
+      <div>
+        <Layout>
+          
+        </Layout>
+      </div>
+    );
+}
+
+export default Index
